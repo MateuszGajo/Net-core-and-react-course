8ddbb3cbe01a636dc045c7ae3e98e52714eaec5f
Mateusz <mateusz.gajo9@wp.pl>
2021-01-28 11:32:09 +0100

    add crud functionality

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+interface IProps {
+  openCreateForm: () => void;
+}
+
+const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          .Net
+        </Menu.Item>
+        <Menu.Item name="Net Core" />
+        <Menu.Item>
+          <Button onClick={openCreateForm} positive content="Create activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
